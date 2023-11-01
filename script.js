7c0c32154cd44d8e0ad15cd5759b982ffd262c1f
Wejustflesh <132151335+Wejustflesh@users.noreply.github.com>
2023-11-01 18:12:09 +0000

    Add files via upload

--- script.js
+++ script.js
@@ -1,7 +1,13 @@
+// Получаем кнопку меню и меню-контент
 const menuButton = document.querySelector('.menu-button');
 const menuContent = document.querySelector('.menu-content');
 
+// Добавляем обработчик клика по кнопке меню
 menuButton.addEventListener('click', () => {
-  menuContent.style.transform = 'scale(1)';
-  menuContent.style.opacity = 1;
+  // По клику меняем стили меню-контента или добавляем/удаляем классы
+  if (menuContent.style.display === 'block') {
+    menuContent.style.display = 'none';
+  } else {
+    menuContent.style.display = 'block';
+  }
 });
